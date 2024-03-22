b459affb37f110611bba4f8c87205ab2efe17871
ACEXRA <rasheed.786ahamed@gmail.com>
2024-03-22 20:31:01 +0530

    auth user structure implementation

--- spring_mongo/src/main/java/com/example/demo/auth/controller/AuthControllerImpl.java
+++ spring_mongo/src/main/java/com/example/demo/auth/controller/AuthControllerImpl.java
@@ -2,26 +2,29 @@ package com.example.demo.auth.controller;
 
 import com.example.demo.auth.service.AuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-public class AuthControllerImpl implements AuthController{
-    @Autowired
-    private AuthServiceImpl authService;
-    @GetMapping("/welcome")
-    public String welcome(){
-        return "welcome";
-    }
-    @GetMapping("/signin")
-    public String signIn(){
-        return authService.signIn();
-    }
+public class AuthControllerImpl{
+        @GetMapping("/welcome")
+        public String welcome() {
+            return "Welcome this endpoint is not secure";
+        }
 
-    @GetMapping("/signout")
-    public String signOut(){
-        return authService.signOut();
-    }
+        @GetMapping("/user/userProfile")
+        @PreAuthorize("hasAuthority('ROLE_USER')")
+        public String userProfile() {
+            return "Welcome to User Profile";
+        }
+
+        @GetMapping("/admin/adminProfile")
+        @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+        public String adminProfile() {
+            return "Welcome to Admin Profile";
+        }
+
 }
